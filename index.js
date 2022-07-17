c6cc97e9b66b38683667e3f215a5c18a5ffee1c0
Mohammad-Abdullah <abdullahofficial2022@gmail.com>
2022-07-17 22:10:59 +0600

    mongodb connected and products route finish

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
-const products = require('./products')
-
-
-
+// const products = require('./products')
+require('dotenv').config()
 
 const port = process.env.PORT || 5000
 
@@ -15,9 +13,34 @@ app.use(express.json())
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
-app.get('/products', async (req, res) => {
-    res.send(products)
-})
+// app.get('/products', async (req, res) => {
+//     res.send(products)
+// })
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.5eij1.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect();
+        const productsCollection = client.db("bestBuy").collection("products");
+
+        app.get('/products', async (req, res) => {
+            const query = {}
+            const cursor = productsCollection.find(query)
+            const products = await cursor.toArray()
+            res.send(products)
+        })
+    } finally {
+        //   await client.close();
+    }
+}
+run().catch(console.dir);
+
+
+
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
