dd3bc46c06593bccaadcbb03a494441c21201f7d
Mohammad-Abdullah <abdullahofficial2022@gmail.com>
2022-08-25 13:39:09 +0600

    lws assignment one tasks added for temporary

---
+++ README.md
@@ -0,0 +1,10 @@
+# Best Buy
+Best Buy is a website for selling products via online
+
+Live Site [https://best-buy-ee029.web.app/](https://best-buy-ee029.web.app/)
+
+## `Features`
+*Coming soon...
+
+## `Technology used`
+*Coming soon...
--- index.js
+++ index.js
@@ -3,10 +3,7 @@ const app = express()
 const cors = require('cors')
 // const products = require('./products')
 require('dotenv').config()
-
 const port = process.env.PORT || 5000
-
-// middleware 
 app.use(cors())
 app.use(express.json())
 
@@ -23,6 +20,14 @@ async function run() {
         await client.connect();
         const productsCollection = client.db("bestBuy").collection("products");
         const ordersCollection = client.db("bestBuy").collection("orders");
+
+        //************************************************************************************** */
+        // this is for my assignment project (todo-lws)
+        //assignment 4
+        const todoCollection = client.db("LWSAssignment").collection("todos");
+
+
+        //************************************************************************************** */
 
         //get all products (mobile)
         app.get('/products', async (req, res) => {
@@ -66,6 +71,81 @@ async function run() {
             const result = await ordersCollection.deleteOne(query);
             res.send(result)
         })
+
+        //************************************************************************************** */
+        // this is for my assignment project (todo-lws)
+        //assignment 4 
+        //get all todos
+        app.get('/todos', async (req, res) => {
+            const todos = await todoCollection.find().toArray()
+            res.send(todos)
+        })
+        //add todo
+        app.post('/todos', async (req, res) => {
+            const todo = req.body;
+            const todoCompleted = await todoCollection.insertOne(todo)
+            res.send(todoCompleted)
+        })
+        //delete one todo with id
+        app.delete('/todos/:id', async (req, res) => {
+            const id = req.params.id;
+            console.log(id);
+            const query = { _id: ObjectId(id) }
+            const result = await todoCollection.deleteOne(query);
+            res.send(result)
+        })
+        //update color
+        app.put('/todos/color/:id', async (req, res) => {
+            const updatedColor = req.body
+            console.log(updatedColor);
+            const id = req.params.id;
+            console.log(id);
+            const filter = { _id: ObjectId(id) }
+            const options = { upsert: true }
+            const updateDoc = {
+                $set: {
+                    color: updatedColor.color
+                }
+            }
+            const result = await todoCollection.updateOne(filter, updateDoc, options)
+            res.send(result)
+        })
+        //update status
+        app.put('/todos/status/:id', async (req, res) => {
+            const updatedStatus = req.body
+            const id = req.params.id;
+            const filter = { _id: ObjectId(id) }
+            const options = { upsert: true }
+            const updateDoc = {
+                $set: {
+                    completed: updatedStatus.completed
+                }
+            }
+            const result = await todoCollection.updateOne(filter, updateDoc, options)
+            res.send(result)
+        })
+        //update text
+        app.put('/todos/text/:id', async (req, res) => {
+            const updatedText = req.body
+            const id = req.params.id;
+            const filter = { _id: ObjectId(id) }
+            const options = { upsert: true }
+            const updateDoc = {
+                $set: {
+                    text: updatedText.text
+                }
+            }
+            const result = await todoCollection.updateOne(filter, updateDoc, options)
+            res.send(result)
+        })
+
+
+
+
+
+        //***************************************************************************************** */
+
+
 
     } finally {
         //   await client.close();
