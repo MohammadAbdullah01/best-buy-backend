3363e035256ac22df563b67f91e1a696b3731db3
Mohammad-Abdullah <abdullahofficial2022@gmail.com>
2022-08-25 14:07:48 +0600

    issue solved for lws assignment one

--- index.js
+++ index.js
@@ -89,7 +89,6 @@ async function run() {
         //delete one todo with id
         app.delete('/todos/:id', async (req, res) => {
             const id = req.params.id;
-            console.log(id);
             const query = { _id: ObjectId(id) }
             const result = await todoCollection.deleteOne(query);
             res.send(result)
